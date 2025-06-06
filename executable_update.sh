bd48c6cc875f6faa42211699a0436f258e30edc1
Luke Harris <lkhrs@users.noreply.github.com>
2025-06-06 07:33:37 -0500

    Add Node version check

--- executable_update.sh
+++ executable_update.sh
@@ -2,6 +2,10 @@
 brew upgrade
 pnpm self-update
 pnpm update -g --latest
-tldr --update
 pnpm approve-builds -g
+echo "Latest installed Node version:"
+pnpm env list | tail -n 1
+echo "Latest available Node version:"
+pnpm env list --remote | tail -n 1
+tldr --update
 chezmoi update
