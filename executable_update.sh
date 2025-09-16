03bf58c35b57e92fc02881d01fa0794bb5507666
Luke Harris <lkhrs@users.noreply.github.com>
2025-09-15 20:00:00 -0500

    Add check for pnpm

--- executable_update.sh
+++ executable_update.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 brew upgrade
-pnpm self-update
-pnpm update -g --latest
-pnpm approve-builds -g
-echo "Latest installed Node version:"
-pnpm env list | tail -n 1
-echo "Latest available Node version:"
-pnpm env list --remote | tail -n 1
+if command -v pnpm >/dev/null 2>&1; then
+    pnpm self-update
+    pnpm update -g --latest
+    pnpm approve-builds -g
+    echo "Latest installed Node version:"
+    pnpm env list | tail -n 1
+    echo "Latest available Node version:"
+    pnpm env list --remote | tail -n 1
+fi
 tldr --update
 chezmoi update
 gh extension upgrade --all
